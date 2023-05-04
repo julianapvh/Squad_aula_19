a7005ac2a0ced651c8942f798757e95949cab771
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-04 16:43:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from funcoes import *
+
+num1 = int(input('Insira o primeiro número: '))
+num2 = int(input('Insira o segundo número: '))
+
+soma(num1,num2)
+subtracao(num1,num2)
+multiplicacao(num1,num2)
+divisao(num1,num2)
+divisao_semsobra(num1,num2)
+divisao_comsobra(num1,num2)
+
+print('Muito obrigado por ultilizar nosso sistema')
